7f6a0e591c2c41c5fe8ce81abcf132cc36ea872d
ArturoHerrera <joseherreravi92@gmail.com>
2022-09-13 22:24:17 -0600

    Se forza portrait mode como orientacion.

--- app/src/main/java/com/arthur/joke_app/ui/JokeAppApp.kt
+++ app/src/main/java/com/arthur/joke_app/ui/JokeAppApp.kt
@@ -1,9 +1,12 @@
 package com.arthur.joke_app.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.arthur.joke_app.navigation.JokeAppNavGraph
 import com.arthur.joke_app.ui.theme.JokeAppTheme
@@ -17,7 +20,7 @@ fun AndroidTemplateApp() {
     JokeAppTheme {
         val navController = rememberNavController()
 
-        Scaffold {
+        Scaffold { _ ->
             JokeAppNavGraph(
                 navController = navController
             )
