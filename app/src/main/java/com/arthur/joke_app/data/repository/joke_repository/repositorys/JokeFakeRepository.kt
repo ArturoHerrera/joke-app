116d9a8036ce79edd8ece26342ec740b9e5efbd8
ArturoHerrera <joseherreravi92@gmail.com>
2022-10-19 12:12:06 -0600

    Se mueve a su carpeta correspondiente el jokeFakeRepository.

--- app/src/main/java/com/arthur/joke_app/data/repository/joke_repository/repositorys/JokeFakeRepository.kt
+++ app/src/main/java/com/arthur/joke_app/data/repository/joke_repository/repositorys/JokeFakeRepository.kt
@@ -1,11 +1,31 @@
 package com.arthur.joke_app.data.repository.joke_repository.repositorys
 
+import com.arthur.joke_app.data.remote.dto.JokeFlags
 import com.arthur.joke_app.data.remote.dto.JokeResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class JokeFakeRepository : JokeTasks {
-
-    override suspend fun getJoke(): Flow<Pair<String?, JokeResponse?>> {
-        TODO("Not yet implemented")
+    override suspend fun getJoke(): Flow<Pair<String?, JokeResponse?>> = flow {
+        emit(
+            Pair(
+                null, JokeResponse(
+                    error = false,
+                    category = "Misc",
+                    type = "single",
+                    joke = "Una ardilla le pregunta a otra que quiere ser cuando grande, la otra ardilla de responde \\\"Ardila Lülle\\\".",
+                    flags = JokeFlags(
+                        nsfw = true,
+                        religious = false,
+                        political = true,
+                        racist = false,
+                        sexist = false
+                    ),
+                    id = 0,
+                    safe = false,
+                    lang = "es"
+                )
+            )
+        )
     }
 }
--- app/src/test/java/com/arthur/joke_app/HomeViewModelTest.kt
+++ app/src/test/java/com/arthur/joke_app/HomeViewModelTest.kt
@@ -1,12 +1,8 @@
 package com.arthur.joke_app
 
-import com.arthur.joke_app.data.remote.dto.JokeFlags
-import com.arthur.joke_app.data.remote.dto.JokeResponse
-import com.arthur.joke_app.data.repository.joke_repository.repositorys.JokeTasks
+import com.arthur.joke_app.data.repository.joke_repository.repositorys.JokeFakeRepository
 import com.arthur.joke_app.ui.screens.home.HomeViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import org.junit.Rule
 import org.junit.Test
 import org.junit.Assert.*
@@ -20,34 +16,7 @@ class HomeViewModelTest {
     @Test
     fun downloadJokeCorrect() {
 
-        class FakeJokeRepository : JokeTasks {
-            override suspend fun getJoke(): Flow<Pair<String?, JokeResponse?>> = flow {
-                emit(
-                    Pair(
-                        null, JokeResponse(
-                            error = false,
-                            category = "Misc",
-                            type = "single",
-                            joke = "Una ardilla le pregunta a otra que quiere ser cuando grande, la otra ardilla de responde \\\"Ardila Lülle\\\".",
-                            flags = JokeFlags(
-                                nsfw = true,
-                                religious = false,
-                                political = true,
-                                racist = false,
-                                sexist = false
-                            ),
-                            id = 0,
-                            safe = false,
-                            lang = "es"
-                        )
-                    )
-                )
-            }
-        }
-
-        val viewModel = HomeViewModel(
-            jokeRepository = FakeJokeRepository()
-        )
+        val viewModel = HomeViewModel(jokeRepository = JokeFakeRepository())
 
         //assert(viewModel.uiState.value.joke == null)
         assert(viewModel.uiState.value.joke != null)
